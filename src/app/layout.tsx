96ed8c65f1459e7506bc86e67bd5261dd12879ea
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-08-02 11:22:04 +0100

    feat: Added a app description SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chat App",
-  description: "",
+  title: "Real-Time Chat Ap",
+  description: "A real-time chat app using Next.js, TypeScript, Express, Socket.io, and Tailwind CSS",
 };
 
 export default function RootLayout({
